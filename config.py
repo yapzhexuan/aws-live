9c9aa9827dfa2585d1427ba75aeb45a4b70fa24b
Yap Zhe Xuan <89828369+yapzhexuan@users.noreply.github.com>
2023-09-07 20:59:18 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cpn8wiomoyeb.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "yapzhexuan-employee"
+customregion = "us-east-1"
+
